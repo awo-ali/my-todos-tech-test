6d677a62440ed24d214f634b10f9172eb0665bfe
awo.ali <awoali@hotmail.co.uk>
2022-06-16 13:10:33 +0100

    ADDS INPUT

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import "./Form.scss"
+
+const Form = (props) => {
+    const {handleChange, handleSubmit} = props
+
+ 
+
+  return (
+    
+    <form className="form" onSubmit={handleSubmit}>
+        <label>
+    <input type="text" placeholder="Add your task here ..." name="{userInput}" onChange={handleChange} className="form__input" />
+    </label>
+    <input type="submit"  className="form__button" value="+" />
+    
+  </form>
+
+  )
+}
+
+export default Form
+
+
