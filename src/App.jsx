3c6460f1b782f78896d51f6505151e1210b126b7
awo.ali <awoali@hotmail.co.uk>
2022-06-16 13:28:22 +0100

    mapping through the array

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,33 @@ import Header from "./container/Header/Header";
 import Note from "./components/Note/Note";
 import Form from "./components/Form/Form";
 
-function App() {
+const App = () => {
   
 const [todos, setTodos] = useState([])
+const [ userInput, setUserInput ] = useState('');
 
-const addTodo = todo =>{
-
+const handleChange = (event) => {
+  event.preventDefault();
+    setUserInput(event.target.value);
 }
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, userInput]) 
+   userInput('')
+}
+
+const todoList = (userInput) => { return userInput.map((userInput) => (
+  <Note/>
+  ))
+};
+
   return (
     <div className="app">
       <Header />
-      <Form />
+      <Form handleChange={handleChange} handleSubmit={handleSubmit}/>
       <Note userInput={userInput} />
-    
+   
       <p className="app__welcomeText">
         Nothing to see here yet ... Add a task in the field above!
       </p>
