08a30d4177fb79cc8656342c821d25e8f417725c
awo.ali <awoali@hotmail.co.uk>
2022-06-16 11:30:06 +0100

    created header and main container

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.scss"
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>My Todo's </h1>
+        <button className='header__button'>Reset</button>
+    </div>
+  )
+}
+
+export default Header
