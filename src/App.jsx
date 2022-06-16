79b17af2f83ab3385b1bbdace9025930ff1e3d31
awo.ali <awoali@hotmail.co.uk>
2022-06-16 13:04:25 +0100

    add todo function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.scss";
+import React, { useState } from "react";
 import Header from "./container/Header/Header";
+import Note from "./components/Note/Note";
+import Form from "./components/Form/Form";
 
 function App() {
+  
+const [todos, setTodos] = useState([])
+
+const addTodo = todo =>{
+
+}
   return (
     <div className="app">
       <Header />
-      <div class="app__inputWithButton">
-        <input type="text" placeholder="Add your task here ..." name="input"  className="app__inputWithButton-input" />
-        <button className="app__inputWithButton-button">+</button>
-      </div>
+      <Form />
+      <Note userInput={userInput} />
+    
       <p className="app__welcomeText">
         Nothing to see here yet ... Add a task in the field above!
       </p>
